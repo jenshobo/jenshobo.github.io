e37ec4a4fceadf1bad72b969b8b38112093c315c
jenshobo <46562488+jenshobo@users.noreply.github.com>
2022-06-14 13:10:19 +0200

    php is not suported sadly

--- Layout/Header.php
+++
@@ -1,8 +0,0 @@
-<h1>
-    <?php echo "Portfolio Jens Sipke Kees Hobo"; ?>
-</h1>
-<h2>
-    <a data-auto-recognition="true" href="mailto:jenshobo@gmail.com" style='text-decoration: none;'>jenshobo@gmail.com</a>
-    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-    <?php echo "06 3904 0305"; ?>
-</h2>
