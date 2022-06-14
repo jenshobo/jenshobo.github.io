2dea0e16c38562604c43d5e28fac42cc21b13d1c
jenshobo <46562488+jenshobo@users.noreply.github.com>
2022-06-14 17:57:33 +0200

    javascript

--- Content/raycaster.js
+++ Content/raycaster.js
@@ -1,1 +1,22 @@
-document.write("test");
+function drawGrid(){
+    var c = document.getElementById("topview");
+    var ctx = c.getContext("2d");
+    for (var index = 50; index < 550; index += 50) {
+        ctx.beginPath();
+        ctx.moveTo(index, 0);
+        ctx.lineTo(index, 550);
+        ctx.stroke();
+
+        ctx.beginPath();
+        ctx.moveTo(0, index);
+        ctx.lineTo(550, index);
+        ctx.stroke();
+    }
+}
+
+function drawPlayer(){
+    var c = document.getElementById("topview");
+    var ctx = c.getContext("2d");
+}
+
+drawGrid();
