acab29275dfad6e06151ae9ae10862cfe882e0c7
jenshobo <46562488+jenshobo@users.noreply.github.com>
2023-07-11 21:24:20 +0200

    added backgrounds, fixed bug

--- External/Script/Button.js
+++ External/Script/Button.js
@@ -3,6 +3,15 @@ var all = document.getElementsByTagName("input");
 for (var i=0, max=all.length; i < max; i++) {
      all[i].onclick = function(){
         url = this.getAttribute('title');
+
+        // check for 'http' to determine if link is internal or external
+        // external links are to be opened on a new browser window and igone transition
+        // internal links are to replace the current page
+        if (url.includes("http")){
+            window.open(url);
+            return;
+        }
+
         let start = Date.now(); // remember start time
 
         let timer = setInterval(function() {
@@ -12,15 +21,7 @@ for (var i=0, max=all.length; i < max; i++) {
         if (timePassed >= 1000) {
             clearInterval(timer); // finish the animation after 1 seconds
             
-            // check for 'http' to determine if link is internal or external
-            // external links are to be opened on a new browser winow
-            // internal links are to replace the current page
-            if (url.includes("http")){
-                window.open(url); // open external link
-            }
-            else{
-                window.open(url, "_self"); // open internal link
-            }
+            window.open(url, "_self"); // open internal link
         }
 
         // draw the animation at the moment timePassed
